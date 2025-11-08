29a8c212bf22ee7dd162a1aa6c7213dee77ad6b7
Hugues Vazquez <huguesvazquez@gmail.com>
2025-11-08 19:10:40 +0100

    maj db

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://mtg-react-ofc8.onrender.com")); 
+        configuration.setAllowedOrigins(Arrays.asList("https://mtg-react-ofc8.onrender.com", "localhost:3000")); 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-XSRF-TOKEN"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "X-XSRF-TOKEN"));
